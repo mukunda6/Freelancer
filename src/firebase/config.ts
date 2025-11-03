0a51193fe29f3eef5c431d647df019f2d3b703de
238R1A6733 KUNDEM PRANAV VENKAT <238r1a6733@cmrec.ac.in>
2025-11-03 21:23:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9338804061-dc255",
+  "appId": "1:988158237335:web:93aeaa233884e24ce8a7d2",
+  "apiKey": "AIzaSyCpVHELmIS3bdWL1YbTRKdPknwjqcyjJTc",
+  "authDomain": "studio-9338804061-dc255.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "988158237335"
+};
